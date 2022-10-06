949b6e544b2089d1c148a2e5fb53492bd886c551
giorgioverzicco <giorgioverzicco00@gmail.com>
2022-10-06 14:19:56 +0200

    end of exercise

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -52,4 +52,59 @@ public class PizzaController : Controller
 
         return RedirectToAction(nameof(Index));
     }
+    
+    public IActionResult Update(int id)
+    {
+        Pizza? pizza = _ctx.Pizzas.FirstOrDefault(x => x.Id == id);
+
+        if (pizza is null)
+        {
+            return View("Error");
+        }
+
+        return View(pizza);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Update(int id, Pizza newPizza)
+    {
+        Pizza? pizza = _ctx.Pizzas.FirstOrDefault(x => x.Id == id);
+
+        if (pizza is null)
+        {
+            return NotFound();
+        }
+
+        if (!ModelState.IsValid)
+        {
+            return View(newPizza);
+        }
+
+        pizza.Name = newPizza.Name;
+        pizza.Description = newPizza.Description;
+        pizza.Photo = newPizza.Photo;
+        pizza.Price = newPizza.Price;
+        
+        _ctx.SaveChanges();
+        
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Delete(int id)
+    {
+        Pizza? pizza = _ctx.Pizzas.FirstOrDefault(x => x.Id == id);
+
+        if (pizza is null)
+        {
+            return NotFound();
+        }
+
+        _ctx.Remove(pizza);
+        _ctx.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
 }
